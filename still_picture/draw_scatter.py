4f89039c5a78eae59895192b415e0854b7772360
lily <lilinyu861@outlook.com>
2018-12-04 22:28:30 +0800

    add scatter

--- still_picture/draw_scatter.py
+++ still_picture/draw_scatter.py
@@ -1,3 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# 简单的散点图
+plt.title("this is a simple diagram")
+plt.xlim(xmax=7, xmin=0)
+plt.ylim(ymax=7, ymin=0)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.plot([1, 2, 3], [4, 5, 6], 'ro')
+plt.show()
